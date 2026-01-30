f1508db636551515cae61f471b09a2368ced835f
Pee <=>
2026-01-30 17:12:21 +0700

    add folder

---
+++ week9-1/app2.py
@@ -0,0 +1,11 @@
+from models.bankaccount import BankAccount
+
+
+my_account = BankAccount(1000)
+your_account = BankAccount(500)
+
+our_account = my_account + your_account
+#our_account = our_account - your_account
+our_account -= your_account
+print(our_account)
+#print(my_account)123134
---
+++ week9-1/models/person.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, pid, name, age, ):
+        self.pid = pid
+        self.name = name
+        self.age = age
+        # Additional methods and attributes can be added here as needed
+        # Additional methods and attributes can be added here as needed
